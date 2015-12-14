405bf6a54b4659e244e425ecc742b19f2d657d07
Dong Yuwei <newdongyuwei@gmail.com>
2015-12-14 14:53:56 +0800

    exclude the translation which exists in messages.pot: avoid translation repeatedly or conflict

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 var fs = require('fs');
 var path = require('path');
 var OpenCC = require('opencc');
+var po2json = require('po2json');
 
 
-if (process.argv.length !== 3) {
-    console.log("Usage: " + __filename + " path/to/html/template/directory");
+if (process.argv.length < 4) {
+    console.log("Usage: " + __filename + " path/to/html/template/directory  path/to/messages.pot");
     process.exit(1);
 }
 
 var dir = process.argv[2];
+
+var translated = po2json.parseFileSync(process.argv[3], {
+    fuzzy: true,
+    format: 'mf'
+});
 
 var content = [];
 var items = fs.readdirSync(dir);
@@ -31,10 +37,12 @@ while ((results = reg.exec(content)) !== null) {
 
 var tw = [], zh = [];
 for(var key in map){
-    tw.push('msgid "' + key + '"\nmsgstr "' + map[key] + '"');
-    zh.push('msgid "' + key + '"\nmsgstr ""');
+    if(!(key in translated)){
+        tw.push('msgid "' + key + '"\nmsgstr "' + map[key] + '"');
+        zh.push('msgid "' + key + '"\nmsgstr ""');
+    }
 }
 
 fs.writeFileSync('./zh.txt', zh.join('\n\n'), 'utf-8');
 fs.writeFileSync('./tw.txt', tw.join('\n\n'), 'utf-8');
-console.log('translations have been write to ./tw.txt ');
+console.log('translations have been write to ./tw.txt and ./zh.txt ');
